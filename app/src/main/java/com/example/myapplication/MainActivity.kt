0c785694ab32c9243425b998d1d9352fe8adb5ae
Daniel Wohlmuth <daniel.wohlmuth@fitapp.info>
2024-01-13 15:14:24 +0100

    Added OnClickListener.

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+        btnLogin.setOnClickListener{
+            Toast.makeText(this, getString(R.string.signed_in), Toast.LENGTH_LONG).show()
+        }
     }
 }
