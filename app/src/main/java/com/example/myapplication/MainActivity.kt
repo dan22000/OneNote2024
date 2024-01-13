1c78cc109c8d7d5461a6051f7f98e6c28d1585ec
Daniel Wohlmuth <daniel.wohlmuth@fitapp.info>
2024-01-13 15:37:58 +0100

    Added Animation.

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +15,10 @@ class MainActivity : AppCompatActivity() {
         val btnLogin = findViewById<Button>(R.id.btnLogin)
         btnLogin.setOnClickListener{
             Toast.makeText(this, getString(R.string.signed_in), Toast.LENGTH_LONG).show()
+
+            val ivIcon = findViewById<ImageView>(R.id.ivIcon)
+            val rotation = AnimationUtils.loadAnimation(this, R.anim.rotate)
+            ivIcon.startAnimation(rotation)
         }
     }
 }
