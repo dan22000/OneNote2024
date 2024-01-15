394a6f20c2109296eaff0ba77cfd0bd7949a6cd7
Daniel Wohlmuth <daniel.wohlmuth@fitapp.info>
2024-01-15 15:47:54 +0100

    Added Comments.

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -13,14 +13,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Find views by ID
         val btnLogin = findViewById<Button>(R.id.btnLogin)
+
+        // Rotate Icon
+        val ivIcon = findViewById<ImageView>(R.id.ivIcon)
+        val rotation = AnimationUtils.loadAnimation(this, R.anim.rotate)
+        ivIcon.startAnimation(rotation)
+
+        // Set OnClickListener
         btnLogin.setOnClickListener{
+            // Show Signed-In-Message
             Toast.makeText(this, getString(R.string.signed_in), Toast.LENGTH_LONG).show()
 
-            val ivIcon = findViewById<ImageView>(R.id.ivIcon)
-            val rotation = AnimationUtils.loadAnimation(this, R.anim.rotate)
-            ivIcon.startAnimation(rotation)
-
+            // Open ListActivity
             val intent = Intent(this, ListActivity::class.java)
             startActivity(intent)
         }
