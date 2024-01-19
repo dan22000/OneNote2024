dbbf50a60196a37d688259704d2ced1c0e83578b
Daniel Wohlmuth <daniel.wohlmuth@fitapp.info>
2024-01-19 14:12:14 +0100

    Added Toolbar.

--- app/src/main/java/com/example/myapplication/ListActivity.kt
+++ app/src/main/java/com/example/myapplication/ListActivity.kt
@@ -5,11 +5,17 @@ import android.os.Bundle
 import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 
 class ListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
+
+        setSupportActionBar(findViewById(R.id.tbMain))
+
+        val title = findViewById<TextView>(R.id.tvTitle)
+        title.text = "dasdasdasdasd"
     }
 }
