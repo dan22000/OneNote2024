160fd666deadd327a58ed734df50ccb8fd5f122f
Daniel Wohlmuth <daniel.wohlmuth@fitapp.info>
2024-01-20 11:45:01 +0100

    Removed unused textViews. Added list_item_view.xml.

--- app/src/main/java/com/example/myapplication/ListActivity.kt
+++ app/src/main/java/com/example/myapplication/ListActivity.kt
@@ -11,30 +11,19 @@ import android.widget.TextView
 
 class ListActivity : AppCompatActivity() {
 
-    private var tvTitle: TextView? = null
-    private var tvMessage: TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
 
         setSupportActionBar(findViewById(R.id.tbMain))
 
-        tvTitle = findViewById(R.id.tvTitle)
-        tvMessage = findViewById(R.id.tvMessage)
         val lvNotes = findViewById<ListView>(R.id.lvNotes)
-        val notes = arrayOf(
-            "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test",
-            "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test", "test"
-        )
-        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, notes)
-        lvNotes.adapter = arrayAdapter
     }
 
     override fun onResume() {
         super.onResume()
 
-        tvTitle!!.text = Preferences(this).getNoteTitle()
-        tvMessage!!.text = Preferences(this).getNoteMessage()
+        // TODO Update View
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
